2a65516edf6b99f157d39e51cbf5181767131879
Kiserus <murzaev_nurislam@mail.ru>
2023-04-27 23:04:35 +0300

    Update MenuActivity.java

--- Project/app/src/main/java/game/space/project/MenuActivity.java
+++ Project/app/src/main/java/game/space/project/MenuActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.ActionMenuView;
 import android.widget.Button;
 import android.view.Menu;
@@ -22,6 +23,9 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Window w = getWindow();
+        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         ImageView goToMainActivity = findViewById(goToSecond);
         goToMainActivity.setOnTouchListener(new View.OnTouchListener() {
